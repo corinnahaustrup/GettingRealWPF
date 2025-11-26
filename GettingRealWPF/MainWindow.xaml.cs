70846a168ddc7bb8d13a95b4304d031c9b742de9
Corinna Haustrup <corinnahaustrup@gmail.com>
2025-11-26 13:09:14 +0100

    for contactinfo

--- GettingRealWPF/MainWindow.xaml.cs
+++ GettingRealWPF/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace GettingRealWPF
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }

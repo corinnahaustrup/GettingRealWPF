34e7f180dd874938d19ea72b5525b97f86042519
Corinna Haustrup <corinnahaustrup@gmail.com>
2025-11-27 10:01:31 +0100

    halv færdigt booking, haidresser, customer, service

---
+++ GettingRealWPF/Models/Booking.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealWPF.Models
+{
+    public class Booking
+    {
+        //private felter
+        private int bookingId;
+        private DateTime dateTime;
+        private string status;
+
+        //constructor 
+        public Booking(int bookingId, DateTime dateTime, string status)
+        {
+            this.bookingId = bookingId;
+            this.dateTime = dateTime;
+            this.status = status;
+        }
+        public string GetStatus()
+        {
+            return status; 
+        }
+        public bool Confirm()
+        {
+            if (Validate())
+            {
+                status = "Confirmed";
+                return true;
+            }
+        }
+    }
+}
---
+++ GettingRealWPF/Models/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealWPF.Models
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public TimeSpan DurationMinutes { get; set; }
+    //Constructors
+    public Service(int serviceId, decimal price, TimeSpan durationMinutes) 
+        {
+        }
+    }
+}
